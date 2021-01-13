9af88083adc2bb974950f6556451d523b90bfe2d
splacik992 <r.paliwoda992@gmail.com>
2021-01-13 08:29:46 +0100

    comment event listener(heroku duplicate event listener methods)

--- src/main/java/pl/coderslab/charity/service/CategoryService.java
+++ src/main/java/pl/coderslab/charity/service/CategoryService.java
@@ -26,14 +26,14 @@ public class CategoryService {
         this.categoryToCategoryDtoMapper = categoryToCategoryDtoMapper;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void fillDonationDB(){
-        categoryRepository.save(new Category(1L,"ubrania"));
-        categoryRepository.save(new Category(2L,"zabawki"));
-        categoryRepository.save(new Category(3L,"kuchnia"));
-        categoryRepository.save(new Category(4L,"rtv i agd"));
-        categoryRepository.save(new Category(5L,"inne"));
-    }
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void fillDonationDB(){
+//        categoryRepository.save(new Category(1L,"ubrania"));
+//        categoryRepository.save(new Category(2L,"zabawki"));
+//        categoryRepository.save(new Category(3L,"kuchnia"));
+//        categoryRepository.save(new Category(4L,"rtv i agd"));
+//        categoryRepository.save(new Category(5L,"inne"));
+//    }
 
     @Transactional
     public List<CategoryDto> getCategoryListFromDB(){
